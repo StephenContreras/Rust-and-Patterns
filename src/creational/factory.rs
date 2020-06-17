6c2b10c97ca2b386202691bda9ddd65fad7ac239
Stephen Contreras <stephenjcontreras@gmail.com>
2020-06-16 21:54:43 -0500

    Mainly setting up structure

---
+++ src/creational/factory.rs
@@ -0,0 +1,27 @@
+pub trait Beverage{
+    fn temperature(&self) -> i32;
+    fn is_hot(&self) -> bool{
+        if self.temperature() > 80 {
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
+}
+
+pub struct Tea {
+    temperature : i32
+}
+
+impl Beverage for Tea{
+    fn temperature(&self) -> i32{
+        self.temperature
+    }
+}
+
+pub fn run_factory(){
+    println!("FACTORY");
+}
+
+
--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,14 @@
+mod creational;
+
 fn main() {
     let args: Vec<String> = std::env::args().collect();
-    println!("{:?}", args);
-    let choice :&str = &args[1];
-    println!("{:?}", choice);
+    if args.len() > 1 {
+        // let choice  = &args[1].to_lowercase().as_str();
+        match args[1].to_lowercase().as_str() {
+            "factory" => creational::factory::run_factory(),
+            _ => println!("Unknown input")
+        }
+        
+    }
 
 }
